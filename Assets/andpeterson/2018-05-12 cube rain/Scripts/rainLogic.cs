f2970101b4e4aa48beef29e88c5b30eefa46df1e
Andrew Peterson <andpeterson.ap@gmail.com>
2018-07-01 22:52:41 -0700

    updated rainfall to be more variabled, adding rainfall and lifespan variables

--- Assets/andpeterson/2018-05-12 cube rain/Scripts/rainLogic.cs
+++ Assets/andpeterson/2018-05-12 cube rain/Scripts/rainLogic.cs
@@ -4,28 +4,51 @@ using UnityEngine;
 
 public class rainLogic : MonoBehaviour {
 
+	//Enumerations
+	public enum RainLevels{Low = 10, Moderate = 5, Heavy = 1};
+
+
+	//Rain asset
 	public Transform raindrop_prefab;
 
+	//Rain variables
+	public RainLevels rainfall;
+	public int raindropLifespan;
+
 	//Stores postion on rotation of new raindrop
-
-
 	private Vector3 position;
 	private Quaternion rotation;
+	 
 
 	//holds GameObjects for all raindrops
-	private Queue<Transform> rain_stack = new Queue<Transform>(200);
+	private Queue<GameObject> rain_stack;
+
+	//how often rain is created
+	private float rain_speed;
+
+	//amount of rain in existance at once
+	private int max_rain;
+
 
 	// Use this for initialization
 	void Start () {
+		//INIT RAIN VARAIBLES
+		//rain speed is  0.01f is the offset
+		rain_speed = (float)rainfall * 0.01f;
+		//max rain = rain drops falling in a sec * lifespan of raindrops
+		max_rain = (int)(1 / rain_speed * raindropLifespan);
+		print ("Length: " + (int)((1 / rain_speed) * raindropLifespan));
+		rain_stack = new Queue<GameObject> (max_rain) ;
+		InvokeRepeating ("UpdateRain", 0f, rain_speed);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void UpdateRain () {
 		position = new Vector3 (Random.Range (-5.0f, 5.0f), 5, Random.Range (-5.0f, 5.0f));
 		rotation = Random.rotation;
-		rain_stack.Enqueue(Instantiate(raindrop_prefab, position, rotation));
-		if (rain_stack.Count >= 200) {
-			Destroy(rain_stack.Dequeue ().gameObject);
+		rain_stack.Enqueue(Instantiate(raindrop_prefab, position, rotation).gameObject);
+		print (rain_stack.Count);
+		if (rain_stack.Count >= max_rain - 1) {
+			Destroy(rain_stack.Dequeue ());
 		}
 	}
 }
