aa5b4ccf88a91aaddcfb01afc58f41889a192f1d
Andrew Peterson <andpeterson.ap@gmail.com>
2018-07-07 16:19:42 -0700

    Wrote VelocityTrigger script and added thunderclap sfx, finished project

---
+++ Assets/andpeterson/2018-07-04 super punch/Scripts/VelocityTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class VelocityTrigger : MonoBehaviour {
+
+	//CONSTS
+	//amount of frames to be stored
+	private const int RECORD_DEPTH = 3;
+
+	private float cooldown = 1;
+
+	//DATA STRUCTURES
+	//positions object previously was
+	private Queue<Vector3> prevPositions;
+
+	//PUBLIC VARIABLES
+	AudioSource audio;
+
+	//VARIABLES TO BE USED
+	//velocity of the object
+	private float velocity = 0;
+	//current position of the object
+	private Vector3 currentPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		//init Queue of previous positions to have a length of RECORD_DEPTH
+		prevPositions = new Queue<Vector3>(RECORD_DEPTH);
+
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = this.transform.position;
+		if (prevPositions.Count == RECORD_DEPTH) {
+			velocity = ((currentPosition - prevPositions.Dequeue()).magnitude);
+		}
+		prevPositions.Enqueue (currentPosition);
+		if (cooldown <= 0) {
+			if (velocity >= 0.12f) {
+				audio.Play ();
+				cooldown = 1;
+			}
+		} else {
+			cooldown -= Time.deltaTime;
+		}
+	}
+}
